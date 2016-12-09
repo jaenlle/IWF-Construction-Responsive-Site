4c8865a9bedc1d4f98318b19a2a8431e0f967f45
Julio Aenlle <gmg8r251@ufl.edu>
2016-12-08 21:52:29 -0500

    Add slicknav responsive navigation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-  $('.bxslider').bxSlider(
-      {
-          auto: true,
-          infiniteLoop: true,
-          responsive: true,
-      }
-  );
+    $('.bxslider').bxSlider(
+        {
+            auto: true,
+            infiniteLoop: true,
+            responsive: true,
+        }
+    );
+
+    $(function(){
+        $('#menu').slicknav({
+            label: '',
+            duration: 600,
+            prependTo: '#container',
+        });
+
+        $('.slicknav_menu').prepend('<img class="responsive-logo" src="images/iwf-logo.png" title="IWF Construction & Glazing" alt="South Florida construction and glazing services" />');
+    });
+
+
 });
